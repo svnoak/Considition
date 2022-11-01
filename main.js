8b67a1541db3256206bffa2c46b25c2145caa0e0
svnoak <kimpfimp@posteo.net>
2022-11-01 20:45:34 +0100

    lala

--- main.js
+++ main.js
@@ -1,22 +1,41 @@
 ﻿const api = require("./api");
 const solver = require("./solver");
 
-const apiKey = ""; //TODO: Your api key here
+const apiKey = "4d623da4-d9fe-403a-4806-08da97ce1ad5"; //TODO: Your api key here
 //The different map names can be found on considition.com/rules
-const currentMap = "training1"; //Todo: Your map choice here
+const currentMap = "FancyVille"; //Todo: Your map choice here
 
 // TODO: You bag type choice here. Unless changed, the bag type 1 will be selected.
-const bag_type = 1
+const consumerPrice = 0.9; // percent of bag
+
+const bagTypes = [1,2,3,4,5];
+const recycleRefundChoice = [true, false];
+
 
 async function main(){
-    let response = await api.getMap(apiKey, currentMap)
-    let days = 31;
-    if (currentMap != "training1" && currentMap == "training2") {
-        days = 365;
+    let progsubs = [];
+    for (const bagType of bagTypes) {
+        for (const recycle of recycleRefundChoice) {
+            for (let i = 1.1; i <= 50; i+0.5) {
+                for (let j = 0.1; j < 1; j+0.2) {
+                    progsubs.push({bagType: bagType, recycle: recycle, bagPrice: i, refund: j});
+                }
+            }
+        }
     }
-    let solution = solver.solve(response, bag_type, days);
-    let score = await api.submitGame(apiKey, currentMap, solution);
-    console.log(score);
+
+    console.log("Progsubs finished");
+
+    let days = 31;
+    let submissions = [];
+        let response = await api.getMap(apiKey, currentMap);
+        for (const sub of progsubs) {
+            let solution = solver.solve(response, sub, days);
+            let score = await api.submitGame(apiKey, currentMap, solution);
+            submissions.push({solution: {...solution}, score: score});
+        }
+
+        submissions.forEach(e => console.log(e.solution.bagType + " " + e.score.score));
 }
 
 main();
--- solver.js
+++ solver.js
@@ -13,34 +13,34 @@ let solution = {};
  * @returns A solution with bag orders per day and other attributes.
  */
 
-function solve(map, bagType, days) {
-    solution.recycleRefundChoice = true
-    solution.bagPrice = 10
-    solution.refundAmount = 1
-    solution.bagType = 1
+function solve(map, sub, days) {
+
+    solution.recycleRefundChoice = sub.recycle;
+    solution.bagPrice = bagType_price[sub.bagType-1] * sub.bagPrice;
+    solution.refundAmount = bagType_price[sub.bagType-1] * sub.refund;
+    solution.bagType = sub.bagType;
     
     solution.orders = []
     for (let day = 0; day < days; day++) {
         solution.orders.push(wasteMoney(bagType, map.companyBudget))
     }
-
-    return solution
+    return solution;
 
 }
 
 // Solution 1: "Spend all money day 1"
 function wasteMoney(bagtype, companyBudget) {
-    return Math.floor(companyBudget / bagType_price[bagtype]);
+    return Math.floor(companyBudget / bagType_price[bagtype-1]);
 }
 
 // Solution 2: "Spend equally money every day"
 function splitMoney(bagtype, companyBudget, days) {
-    return Math.floor(companyBudget / bagType_price[bagtype] / days);
+    return Math.floor(companyBudget / bagType_price[bagtype-1] / days);
 }
 
 // Solution 3: "Everyone get one bag every day"
 function holdMoney(bagtype, companyBudget, population, days) {
-    return Math.floor(companyBudget / bagType_price[bagtype] / population / days);
+    return Math.floor(companyBudget / bagType_price[bagtype-1] / population / days);
 }
 
 module.exports = {
